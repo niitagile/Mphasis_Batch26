34f03a30aa0261949f4575ac86ef188625149f27
niitagile <43024343+niitagile@users.noreply.github.com>
2023-06-27 11:49:33 +0530

    Add files via upload

--- Phase1/FileHandling/MultiObjectSerialization.java
+++ Phase1/FileHandling/MultiObjectSerialization.java
@@ -2,6 +2,7 @@ package FileHandling;
 //When we are writing any Object in file -- Serialization
 //When we read any object from file--deserialization
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -9,6 +10,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -38,6 +41,11 @@ class Movie  implements Serializable {
 		System.out.print("id="+id);
 		System.out.println("\tname="+name);
 	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
 	
 }
 public class MultiObjectSerialization {
@@ -53,6 +61,9 @@ public class MultiObjectSerialization {
 		lmovies.add(m2);
 		try {
 		writeToFile(lmovies);
+		//readFile();
+		//searchMovie();
+		deleteObject();
 		readFile();
 		}
 		catch(IOException e) {
@@ -63,6 +74,26 @@ public class MultiObjectSerialization {
 		}
 		
 
+	}
+	
+	private static void searchMovie() throws IOException, ClassNotFoundException {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter movie name");
+		String mname=sc.nextLine();
+		FileInputStream file=new FileInputStream("Movie.txt");
+		ObjectInputStream os=new ObjectInputStream(file);
+		List<Object> list=(List<Object>)os.readObject();
+		for(Object obj : list) {
+			Movie movie=(Movie)obj;
+			if(movie.getName().equalsIgnoreCase(mname)) {
+			movie.display();
+			break;
+			}
+			
+		}
+		file.close();
+		os.close();
+		
 	}
 
 	private static void readFile() throws IOException, ClassNotFoundException {
@@ -87,6 +118,41 @@ public class MultiObjectSerialization {
 		os.writeObject(lmovies);
 		os.close();
 		file.close();
+		
+		
+	}
+	
+	private static void deleteObject() throws IOException, ClassNotFoundException {
+		//create temp file
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter movie name");
+		String mname=sc.nextLine();
+		FileOutputStream temp=new FileOutputStream("temp.txt");
+		ObjectOutputStream tempos=new ObjectOutputStream(temp);
+		List<Object> templist=new ArrayList<>();
+		//Open file in read mode
+		FileInputStream file=new FileInputStream("Movie.txt");
+		ObjectInputStream os=new ObjectInputStream(file);
+		List<Object> list=(List<Object>)os.readObject();
+		for(Object obj : list) {
+			Movie movie=(Movie)obj;
+			if(movie.getName().equalsIgnoreCase(mname)==false) {
+				templist.add(obj);
+			}
+		}
+		tempos.writeObject(templist);
+		tempos.close();
+		os.close();
+		file.close();
+		temp.close();
+		File f=new File("Movie.txt");
+		f.delete();
+		File ftemp=new File("temp.txt");
+		ftemp.renameTo(f);
+		
+		
+		
+		
 		
 		
 	}
