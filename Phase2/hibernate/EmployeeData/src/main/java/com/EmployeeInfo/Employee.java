8cd4c5a3ecdc547ab3286e600826ad5c49ec29b8
niitagile <43024343+niitagile@users.noreply.github.com>
2023-07-12 11:36:46 +0530

    Add files via upload

---
+++ Phase2/hibernate/EmployeeData/src/main/java/com/EmployeeInfo/Employee.java
@@ -0,0 +1,40 @@
+package com.EmployeeInfo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int rollno;
+	private int empid;
+	@Column(length=20)
+	private String empname;
+	private  float salary;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
+
+}
