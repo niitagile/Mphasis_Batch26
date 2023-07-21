b090bd13fdc0b5668fcc3093072c199ff05d2392
niitagile <43024343+niitagile@users.noreply.github.com>
2023-07-21 10:55:12 +0530

    Add files via upload

---
+++ Phase3/SpringMVCExample/src/main/java/com/springcrud/dao/EmployeeDao.java
@@ -0,0 +1,48 @@
+package com.springcrud.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.springcrud.beans.Employee;
+
+@Component
+public class EmployeeDao {
+	@Autowired
+	JdbcTemplate template;
+	
+	public int insert(Employee emp) {
+		String sql="insert into Employee(eid, name, designation,salary) values(?,?,?,?)";
+		int ans=template.update(sql,emp.getEid(),emp.getName(),emp.getDegisnation(),emp.getSalary());
+		return ans;
+	}
+	
+	public int update(Employee emp) {
+		String sql="update Employee set  name=?, designation=?,salary=? where eid=?";
+		int ans=template.update(sql,emp.getName(),emp.getDegisnation(),emp.getSalary(),emp.getEid());
+		return ans;
+	}
+	
+	public int delete(int id) {
+		String sql="delete from Employee where eid=?";
+		int ans=template.update(sql,id);
+		return ans;
+	}
+	
+	public Employee getEmpId(int id){
+		String sql="select * from Employee where eid=?";
+		Employee emp=template.queryForObject(sql, new Object[] {id},new EmployeeMapper() );
+		
+		
+		return emp;
+	}
+	
+	public List<Employee> getEmployeeDetails(){
+		String sql="select * from Employee";
+		List<Employee> empList=template.query(sql, new EmployeeMapper());
+		return empList;
+	}
+
+}
---
+++ Phase3/SpringMVCExample/src/main/java/com/springcrud/dao/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.springcrud.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.springcrud.beans.Employee;
+
+//RowMapper is pre defined interface to convert resultset records to object
+public class EmployeeMapper implements RowMapper<Employee> {
+
+	@Override
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		Employee employee=new Employee();
+		employee.setEid(rs.getInt("eid"));
+		employee.setName(rs.getString("name"));
+		employee.setDegisnation(rs.getString("designation"));
+		employee.setSalary(rs.getFloat("salary"));
+		
+		
+		return employee;
+	}
+
+}
