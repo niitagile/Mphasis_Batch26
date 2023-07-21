1b0c1d0c4fe94db6fec0828720d76c8770ed7568
niitagile <43024343+niitagile@users.noreply.github.com>
2023-07-21 12:28:36 +0530

    Update EmployeeController.java

--- Phase3/SpringMVCExample/src/main/java/com/springcrud/controller/EmployeeController.java
+++ Phase3/SpringMVCExample/src/main/java/com/springcrud/controller/EmployeeController.java
@@ -17,45 +17,58 @@ import com.springcrud.dao.EmployeeDao;
 public class EmployeeController {
 	@Autowired
 	EmployeeDao dao;
-	//show form to take new record
-	@RequestMapping(value="/empform",method=RequestMethod.GET)
-	public String showForm(Model m) {
+	
+	
+	@RequestMapping(value="/empform",  method=RequestMethod.GET)
+	public String showForm(Model m){
 		m.addAttribute("emp", new Employee());
-		return "empform";//name of jsp form
+		//System.out.println("show Form");
+		return "empform";
 		
 	}
-	@RequestMapping(value="/save",method=RequestMethod.POST)
-	public String save(@ModelAttribute("emp")Employee emp) {
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(@ModelAttribute("emp")Employee emp){
+		//System.out.println(emp.getEid());
 		dao.insert(emp);
 		return "redirect:/viewemp";
-		
 	}
-	@RequestMapping(value="/viewemp" ,method=RequestMethod.GET)
-	public String viewmap(Model m) {
-		List<Employee> empList=dao.getEmployeeDetails();
-		for(Employee emp : empList) {
-			System.out.println(emp.getName());
-		}
-		m.addAttribute(empList);
+	@RequestMapping("/viewemp")
+	public String viewemp(Model m){
+		List<Employee> empList= dao.getEmployeeDetails();
+		/*for(Employee e : empList){
+			System.out.println(e.getEid());
+		}*/
+		m.addAttribute("empList",empList);
 		return "viewemp";
 	}
 	
-	@RequestMapping(value="editeemp/{eid}")
-	public String edit(@PathVariable int eid,Model m) {
+	@RequestMapping(value="/editemp/{eid}")
+	public String edit(@PathVariable int eid, Model m){
+		
 		Employee emp=dao.getEmpId(eid);
-		m.addAttribute(emp);
+		m.addAttribute("emp",emp);
 		return "empeditform";
 	}
-	
-	@RequestMapping(value="editsave", method=RequestMethod.POST)
-	public String editSave(@ModelAttribute("emp") Employee emp) {
+	@RequestMapping(value="/editsave",method=RequestMethod.POST)
+	public String editSave(@ModelAttribute("emp") Employee emp){
+		
+		System.out.println(emp.getEid());
 		dao.update(emp);
 		return "redirect:/viewemp";
 	}
 	
-	@RequestMapping(value="deleteemp/{eid}")
-	public String delete(@PathVariable int eid) {
+	
+	
+	
+	@RequestMapping(value="/deleteemp/{eid}")
+	public String delete(@PathVariable int eid){
 		dao.delete(eid);
 		return "redirect:/viewemp";
-	}
+	}	
+	
+	
+	
+	
+	
 }
