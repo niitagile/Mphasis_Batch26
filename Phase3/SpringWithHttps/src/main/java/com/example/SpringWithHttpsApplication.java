e557d9bcd43d8aa293b9f56a1f129a976f95c1db
niitagile <43024343+niitagile@users.noreply.github.com>
2023-07-28 13:03:11 +0530

    Add files via upload

---
+++ Phase3/SpringWithHttps/src/main/java/com/example/SpringWithHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithHttpsApplication.class, args);
+	}
+
+}
